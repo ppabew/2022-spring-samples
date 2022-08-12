e12308315d8c282f766a0aa4a5003610ce3ec9dc
91274750 <augustman8808@gmail.com>
2022-08-12 12:46:18 +0900

    ⚬ initialize project

---
+++ src/test/java/com/example/samples/SamplesApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("test");
+    }
+
+}
